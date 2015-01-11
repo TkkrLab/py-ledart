5c4e00ca6f6451ff4d9cec13630a0f4f5b2a4a7a
Robert <201292@live.nl>
2015-01-11 20:05:12 +0100

    this

--- configs/default_conf.py
+++ configs/default_conf.py
@@ -1,5 +1,5 @@
-#import all patterns availble for use.
-from patterns.Patterns import *
+#import barberpole
+from patterns.Patterns import BarberpolePattern
 
 led_ceiling_matrix_ip = "10.42.4.12"
 
