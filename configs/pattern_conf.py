4c9479e63641b7c909b6d509575ec8c4053f2b17
Duality4y <201292@live.nl>
2015-03-21 18:22:47 +0100

    made some code nicer need to change color order for simulator.

--- configs/pattern_conf.py
+++ configs/pattern_conf.py
@@ -12,30 +12,24 @@ TARGETS = {
     # pixelMatrix:OldTron(),
     # pixelMatrix:Tron(),
     # local_host:Snake(speed=17),
-    
     # MixedLife() doesn't work atm but will be fixed
     # local_host:MixedLife(),
     # local_host:RandomLife(),
-    pixelMatrix:BlueLife(),
+    pixelMatrix: BlueLife(),
     # raspberrypi:BlueLife(),
-    
     # local_host:SuperPixelBros(),
     # local_host:Pong(speed=5),
     # pixelMatrix:Pong(speed=3),
     # pixelMatrix:Pong(speed=8),
     # pixelMatrix:Snake(),
     # local_host:BlueLife(),
-
     # #needs images. wip still.
     # # pixelMatrix:DisplayPng(),
-
     # pixelMatrix:PlasmaFirst(),
     # pixelMatrix:PlasmaSecond(),
     # michiel_laptop:PlasmaSecond(),
     # pixelMatrix:PlasmaThird(),
-    
     # local_host:RainPattern(chance=0.2),
-    
     # pixelMatrix:GraphicsCircleTest(),
     # pixelMatrix:GraphicsRectTest(),
     # pixelMatrix:GraphicsLineTest(),
