124c6b7f378f63a77736d6e001d1c665873954ba
Duality4y <201292@live.nl>
2015-03-21 21:19:35 +0100

    edited sven.py you don't need a while loop in you generator functions

--- patterns/sven.py
+++ patterns/sven.py
@@ -1,33 +1,25 @@
 from matrix import *
-from Graphics import *  
+from Graphics import *
 import random
-import time
 
 
 class sven(object):
-	def __init__(self):
-		self.graphics = Graphics(matrix_width, matrix_height)
-		self.color = RED
-		BLUE = self.color = RED
-		self.pos = 0,0
-		
-	def generate(self):
-		self.graphics.fill(GREEN)
-		x, y = self.pos
-		#self.graphics.drawLine(matrix_width-x,matrix_height-y,x,y, self.color)
-		b = 0
-		i = 0
-		while(1):
-			#b2 = 0
-			#i = 0
-			while(i<= 10):
-				color2 = randColor()
-				a = random.randint(0,matrix_width)
-				b = random.randint(0,matrix_height)
-				self.graphics.drawPixel(a,b, color2)
-				print i, ":", b
-				i = i+1
+    def __init__(self):
+        self.graphics = Graphics(matrix_width, matrix_height)
+        self.color = RED
+        self.pos = 0, 0
 
-			#time.sleep(1)
-			b = b +1
-			return self.graphics.getSurface()
+    def generate(self):
+        self.graphics.fill(GREEN)
+        x, y = self.pos
+        #self.graphics.drawLine(matrix_width-x,matrix_height-y,x,y, self.color)
+        b = 0
+        i = 0
+        while(i <= 10):
+            color2 = randColor()
+            a = random.randint(0, matrix_width)
+            b = random.randint(0, matrix_height)
+            self.graphics.drawPixel(a, b, color2)
+            i = i+ 1
+        b = b + 1
+        return self.graphics.getSurface()
