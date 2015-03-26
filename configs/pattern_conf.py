85a00c6633ea42872ed4cb61cb49568bac6af500
Duality4y <201292@live.nl>
2015-03-26 21:03:31 +0100

    made more files pep compliant

--- configs/pattern_conf.py
+++ configs/pattern_conf.py
@@ -1,4 +1,4 @@
-#import all patterns availble for use.
+# import all patterns availble for use.
 from patterns.Patterns import *
 
 pixelMatrix = "pixelmatrix"
@@ -7,32 +7,34 @@ michiel_laptop = "192.168.1.199"
 local_host = "127.0.0.1"
 broadcast = "10.42.255.255"
 
+dest = pixelMatrix
+
 TARGETS = {
-    # local_host:BarberpolePattern(),
-    # pixelMatrix:OldTron(),
-    # pixelMatrix:Tron(),
-    # local_host:Snake(speed=17),
+    dest: BarberpolePattern(),
+    dest: OldTron(),
+    dest: Tron(),
+    dest: Snake(speed=17),
     # MixedLife() doesn't work atm but will be fixed
-    # local_host:MixedLife(),
-    # local_host:RandomLife(),
-      pixelMatrix: BlueLife(),
-    # raspberrypi:BlueLife(),
-    # local_host:SuperPixelBros(),
-    # local_host:Pong(speed=5),
-    # pixelMatrix: Pong(speed=3),
-    # pixelMatrix:Pong(speed=8),
-    # pixelMatrix:Snake(),
-    # local_host:BlueLife(),
-    # #needs images. wip still.
-    # # pixelMatrix:DisplayPng(),
-    # pixelMatrix:PlasmaFirst(),
-    # pixelMatrix:PlasmaSecond(),
-    # michiel_laptop:PlasmaSecond(),
-    # pixelMatrix:PlasmaThird(),
-    # local_host:RainPattern(chance=0.2),
-    # pixelMatrix:GraphicsCircleTest(),
-    # pixelMatrix:GraphicsRectTest(),
-    # pixelMatrix:GraphicsLineTest(),
-    # local_host:GraphicsPixelTest(),
-    # local_host:GraphicsDotTest(),
+    dest: MixedLife(),
+    dest: RandomLife(),
+    dest: BlueLife(),
+    dest: BlueLife(),
+    dest: SuperPixelBros(),
+    dest: Pong(speed=5),
+    dest: Pong(speed=3),
+    dest: Pong(speed=8),
+    dest: Snake(),
+    dest: BlueLife(),
+    # needs images. wip still.
+    dest: DisplayPng(),
+    dest: PlasmaFirst(),
+    dest: PlasmaSecond(),
+    dest: PlasmaSecond(),
+    dest: PlasmaThird(),
+    dest: RainPattern(chance=0.2),
+    dest: GraphicsCircleTest(),
+    dest: GraphicsRectTest(),
+    dest: GraphicsLineTest(),
+    dest: GraphicsPixelTest(),
+    dest: GraphicsDotTest(),
 }
--- patterns/Tron.py
+++ patterns/Tron.py
@@ -1,125 +1,132 @@
-from Graphics import *
+from Graphics.Graphics import Graphics, BLUE
+from matrix import matrix_width, matrix_height
+import random
+
 from pygame.locals import *
 import pygame
 
+
 class TronPlayer(object):
-	def __init__(self, color, game, pos=(5,5)):
-		self.game = game
-		self.color = color
-		
-		self.pos = pos
-		self.speed = 1
-		self.deltax,self.deltay = 0,0
-		
-		self.body = []
-		#put our head on our body
-		self.body.append(self.pos)
-		
-		self.moveKeys = [K_LEFT, K_RIGHT, K_UP, K_DOWN]
-		
-		self.debug = False
-		
-	def inputHandling(self, event):
-		pass
-	def bindKey(self, key, action):
-		self.moveKeys[action] = key
-	def update(self, other):
-		x,y = self.pos
-		#update position
-		x += self.deltax
-		y += self.deltay
-		#if tail goes offscreenit appears on the other side.
-		if x >= matrix_width:
-			x = 0
-			self.pos = x,y
-		elif x < 0:
-			x = matrix_width-1
-			self.pos = x,y
-		elif y >= matrix_height:
-			y = 0
-			self.pos = x,y
-		elif y < 0:
-			y = matrix_height-1
-			self.pos = x,y
-		else:
-			self.pos = x,y
-		if self.debug:
-			print self.deltax,self.deltay
-			print x,y
-	def draw(self):
-		for x,y in self.body:
-			self.game.graphics.drawPixel(x,y,self.color)
+    def __init__(self, color, game, pos=(5, 5)):
+        self.game = game
+        self.color = color
+        self.pos = pos
+        self.speed = 1
+        self.deltax, self.deltay = 0, 0
+        self.body = []
+        # put our head on our body
+        self.body.append(self.pos)
+        self.moveKeys = [K_LEFT, K_RIGHT, K_UP, K_DOWN]
+        self.debug = False
+
+    def inputHandling(self, event):
+        pass
+
+    def bindKey(self, key, action):
+        self.moveKeys[action] = key
+
+    def update(self, other):
+        x, y = self.pos
+        # update position
+        x += self.deltax
+        y += self.deltay
+        # if tail goes offscreenit appears on the other side.
+        if x >= matrix_width:
+            x = 0
+            self.pos = x, y
+        elif x < 0:
+            x = matrix_width - 1
+            self.pos = x, y
+        elif y >= matrix_height:
+            y = 0
+            self.pos = x, y
+        elif y < 0:
+            y = matrix_height - 1
+            self.pos = x, y
+        else:
+            self.pos = x, y
+        if self.debug:
+            print(self.deltax, self.deltay)
+            print(x, y)
+
+    def draw(self):
+        for x, y in self.body:
+            self.game.graphics.drawPixel(x, y, self.color)
+
 
 class Tron(object):
-	def __init__(self):
-		self.graphics = Graphics(matrix_width, matrix_height)
-	def generate(self):
-		return self.graphics.getSurface()
+    def __init__(self):
+        self.graphics = Graphics(matrix_width, matrix_height)
 
-class OldTron(object): #version before creating players that handle processing on thier own.
-	def __init__(self):
-		self.graphics = Graphics(matrix_width, matrix_height)
-		
-		self.color = BLUE
-		
-		self.pos = random.randint(1,matrix_width-1), random.randint(1,matrix_height-1)
-		self.speed = 1
-		self.deltax,self.deltay = 0,0
-		
-		self.body = []
-		
-		#add our head to our body :)
-		self.body.append(self.pos)
-		
-		self.player1 = TronPlayer(BLUE, self)
-		self.player1.update(self)
-		self.player1.draw()
-		
-		pygame.init()
-		self.window = pygame.display.set_mode((80,60))
-	def inputHandling(self):
-		pass
-	def update(self):
-		x,y = self.pos
-		#update position
-		x += self.deltax
-		y += self.deltay
-		#if the tail goes offscreen it appears on the other side.
-		if x >= matrix_width:
-			x = 0
-			self.pos = x,y
-		elif x < 0:
-			x = matrix_width-1
-			self.pos = x,y
-		elif y >= matrix_height:
-			y = 0
-			self.pos = x,y
-		elif y < 0:
-			y = matrix_height-1
-			self.pos = x,y
-		else:
-			self.pos = x,y
-		print self.deltax,self.deltay
-		print x,y
-		
-		#look if our "tail is in the way" and only if we have a tail.
-		if len(self.body) > 2:
-			if len(self.body) != len(set(self.body)):
-				print "GameOver!"
-				self.body = [self.pos]
-				self.deltax = 0; self.deltay = 0;
-		#add current point to tail
-		#only if we moved though
-		if self.deltax or self.deltay:
-			self.body.append(self.pos)
-		
-	def draw(self):
-		for x,y in self.body:
-			self.graphics.drawPixel(x,y,self.color)
-	def generate(self):
-		self.graphics.fill(BLACK)
-		self.inputHandling()
-		self.update()
-		self.draw()
-		self.player1.draw()
-		return self.graphics.getSurface()
+    def generate(self):
+        return self.graphics.getSurface()
+
+
+class OldTron(object):
+    # version before creating players that handle processing on thier own.
+    def __init__(self):
+        self.graphics = Graphics(matrix_width, matrix_height)
+        self.color = BLUE
+        x = random.randint(1, matrix_width - 1)
+        y = random.randint(1, matrix_height - 1)
+        self.pos = x, y
+        self.speed = 1
+        self.deltax, self.deltay = 0, 0
+        self.body = []
+        # add our head to our body :)
+        self.body.append(self.pos)
+        self.player1 = TronPlayer(BLUE, self)
+        self.player1.update(self)
+        self.player1.draw()
+        pygame.init()
+        self.window = pygame.display.set_mode((80, 60))
+
+    def inputHandling(self):
+        pass
+
+    def update(self):
+        x, y = self.pos
+        # update position
+        x += self.deltax
+        y += self.deltay
+        # if the tail goes offscreen it appears on the other side.
+        if x >= matrix_width:
+            x = 0
+            self.pos = x, y
+        elif x < 0:
+            x = matrix_width - 1
+            self.pos = x, y
+        elif y >= matrix_height:
+            y = 0
+            self.pos = x, y
+        elif y < 0:
+            y = matrix_height - 1
+            self.pos = x, y
+        else:
+            self.pos = x, y
+        print(self.deltax, self.deltay)
+        print(x, y)
+
+        # look if our "tail is in the way" and only if we have a tail.
+        if len(self.body) > 2:
+            if len(self.body) != len(set(self.body)):
+                print("GameOver!")
+                self.body = [self.pos]
+                self.deltax = 0
+                self.deltay = 0
+        # add current point to tail
+        # only if we moved though
+        if self.deltax or self.deltay:
+            self.body.append(self.pos)
+
+    def draw(self):
+        for x, y in self.body:
+            self.graphics.drawPixel(x, y, self.color)
+
+    def generate(self):
+        self.graphics.fill(BLACK)
+        self.inputHandling()
+        self.update()
+        self.draw()
+        self.player1.draw()
+        return self.graphics.getSurface()
