95435ea19741ad25806d690ba24022d31bbfdd18
Duality4y <201292@live.nl>
2015-03-27 20:31:08 +0100

    tried to make a vu meter but it's not perfect yet.

--- patterns/Vumeter.py
+++ patterns/Vumeter.py
@@ -3,27 +3,49 @@ from Graphics.Graphics import Graphics, BLUE, BLACK
 from Controllers.Controllers import AudioController, translate
 
 
+class VUmeterThree(object):
+    def __init__(self):
+        pass
+
+    def generate(self):
+        pass
+
+
 class VUmetertwo(object):
     def __init__(self):
         self.graphics = Graphics(matrix_width, matrix_height)
-        self.controller = AudioController(channel=1, rate=128000, period=64)
+        self.controller = AudioController(channel=1, rate=256000, period=64)
+        self.color = BLUE
         self.inputlength = matrix_height
         self.inputs = []
-        self.offset = 100
+        self.offset = 13
         self.max = 1500
-        self.lim = 1100
-        self.rate = 500
+        self.lim = self.max
+        self.rate = 2
 
-    def getinputs(self):
-        self.inputs = []
-        for input in range(0, self.inputlength):
+    def getaverageof(self, number):
+        averages = []
+        for i in range(0, number):
             data = self.controller.getinput()
             while(data is None):
                 data = self.controller.getinput()
+            averages.append(data)
+        sums = 0
+        for num in averages:
+            sums += num
+        return int(sums / len(averages))
+
+    def getinputs(self):
+        self.inputs = []
+        sums = 0
+        for input in range(0, self.inputlength):
+            data = self.getaverageof(3)
             self.inputs.append(data)
+            sums += data
+        return sums / len(self.inputs)
 
     def generate(self):
-        self.getinputs()
+        average = self.getinputs()
         self.graphics.fill(BLACK)
         for i, line in enumerate(self.inputs):
             if line > self.max:
@@ -33,14 +55,14 @@ class VUmetertwo(object):
                     self.max = self.lim
                 else:
                     self.max -= int((self.max - self.lim) / self.rate)
-            data = int(translate(line, 0, self.max, 0, matrix_height))
+            length = int(translate(average, 0, self.max, 0, matrix_height))
+            data = int(translate(line, 0, self.max, 0, length))
             if data > 0xff:
-                self.data = 0xff - 1
-            if data == 0:
-                data = 1
-            r = g = b = data + self.offset
-            color = (r, g, b)
-            self.graphics.drawLine(0, i, data - 4, i, color)
+                self.data = 0xff
+            if data > self.max:
+                self.max = data
+            print(data)
+            self.graphics.drawLine(0, i, data - self.offset, i, self.color)
         return self.graphics.getSurface()
 
 
