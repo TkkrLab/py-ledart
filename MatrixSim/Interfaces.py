bc755027c96b0521e4559593da604153109a1e31
Duality4y <201292@live.nl>
2015-03-30 21:44:48 +0200

    been trying to implement a interface for gtk. is difficult. but will go try and implement a interface for opengl.

---
+++ MatrixSim/Interfaces.py
@@ -0,0 +1,95 @@
+import sys
+
+
+class Interface(object):
+    """
+    the basic drawing interface api
+    """
+    def __init__(self, width, height, blocksize):
+        """ Initialize the interface. it know how big the screen is."""
+        # due to physical matrix layout these are switched.
+        self.width = height * blocksize
+        self.height = width * blocksize
+
+    def handleinput(self):
+        """
+        this function handles the inputs. basic q/esc for quit or
+        ctrl-z for quiting. and checking mouse focus.
+        """
+        pass
+
+    def setcaption(self, caption):
+        """ sets the title/caption of the window for the simulator."""
+        pass
+
+    def fill(self, color):
+        """ fills the window with a color."""
+        pass
+
+    def drawblock(self, rect, color, bordercolor, borderwidth=1):
+        """ draws the blocks that are the pixels."""
+        pass
+
+    def update(self):
+        """ updates the interface window. """
+        pass
+
+    def quit(self):
+        """ called when quiting the program."""
+        pass
+
+
+class OpenGlInterface(Interface):
+    def __init__(self, width, height, pixelsize, fullscreen):
+        Interface.__init__(self, width, height, pixelsize)
+
+
+class PygameInterface(Interface):
+    """
+    a abstract drawing interface that uses pygame.
+    """
+    def __init__(self, width, height, blocksize, fullscreen=False):
+        Interface.__init__(self, width, height, blocksize)
+        import pygame
+        self.pygame = pygame
+        self.flags = pygame.DOUBLEBUF | self.pygame.HWSURFACE
+        if fullscreen:
+            self.flags |= self.pygame.FULLSCREEN
+        self.window = self.pygame.display.set_mode((self.width, self.height),
+                                                   self.flags)
+
+    def handleinput(self):
+        for event in self.pygame.event.get():
+            if event.type == self.pygame.QUIT:
+                raise SystemExit
+            if event.type == self.pygame.KEYDOWN:
+                # quit on ctrl-c as if it were in the terminal.
+                # to lazy to press x.
+                lctrlpressed = (self.pygame.key.get_mods() &
+                                self.pygame.KMDED_LCTRL)
+                if event.key == self.pygame.K_c and lctrlpressed:
+                    raise KeyboardInterrupt
+                if event.key == self.pygame.K_q or self.pygame.K_ESCAPE:
+                    raise KeyboardInterrupt
+
+            # check if the mouse pointer is on/in the window.
+            # and if so hide it.
+            focused = self.pygame.mouse.get_focused()
+            self.pygame.mouse.set_visible(not focused)
+
+    def setcaption(self, caption):
+        self.pygame.display.set_caption(caption)
+
+    def fill(self, color):
+        self.window.fill(color)
+
+    def drawblock(self, rect, color, bordercolor, borderwidth=1):
+        self.pygame.draw.rect(self.window, color, rect)
+        # draw a nice little square around so it looks more like a pixel.
+        self.pygame.draw.rect(self.window, bordercolor, rect, borderwidth)
+
+    def update(self):
+        self.pygame.display.update()
+
+    def quit(self):
+        self.pygame.quit()
--- MatrixSim/MatrixScreen.py
+++ MatrixSim/MatrixScreen.py
@@ -8,103 +8,7 @@ import time
 from Pixel import Pixel
 import Graphics.Graphics as Graphics
 import matrix
-
-
-class Interface(object):
-    """
-    the basic drawing interface api
-    """
-    def __init__(self, width, height, blocksize):
-        """ Initialize the interface. it know how big the screen is."""
-        # due to physical matrix layout these are switched.
-        self.width = height * blocksize
-        self.height = width * blocksize
-
-    def handleinput(self):
-        """
-        this function handles the inputs. basic q/esc for quit or
-        ctrl-z for quiting. and checking mouse focus.
-        """
-        pass
-
-    def setcaption(self, caption):
-        """ sets the title/caption of the window for the simulator."""
-        pass
-
-    def fill(self, color):
-        """ fills the window with a color."""
-        pass
-
-    def drawblock(self, rect, color, bordercolor, borderwidth=1):
-        """ draws the blocks that are the pixels."""
-        pass
-
-    def update(self):
-        """ updates the interface window. """
-        pass
-
-    def quit(self):
-        """ called when quiting the program."""
-        pass
-
-
-class GtkInterface(Interface):
-    """
-    abstract darwing interface that uses gtk.
-    """
-    def __init__(self, width, height, blocksize, fullscreen=False):
-        Interface.__init__(self, width, height, blocksize)
-
-
-class PygameInterface(Interface):
-    """
-    a abstract drawing interface that uses pygame.
-    """
-    def __init__(self, width, height, blocksize, fullscreen=False):
-        Interface.__init__(self, width, height, blocksize)
-        import pygame
-        self.pygame = pygame
-        self.flags = self.pygame.DOUBLEBUF | self.pygame.HWSURFACE
-        if fullscreen:
-            self.flags |= self.pygame.FULLSCREEN
-        self.window = self.pygame.display.set_mode((self.width, self.height),
-                                                   self.flags)
-
-    def handleinput(self):
-        for event in self.pygame.event.get():
-            if event.type == self.pygame.QUIT:
-                raise SystemExit
-            if event.type == self.pygame.KEYDOWN:
-                # quit on ctrl-c as if it were in the terminal.
-                # to lazy to press x.
-                lctrlpressed = (self.pygame.key.get_mods() &
-                                self.pygame.KMDED_LCTRL)
-                if event.key == self.pygame.K_c and lctrlpressed:
-                    raise KeyboardInterrupt
-                if event.key == self.pygame.K_q or self.pygame.K_ESCAPE:
-                    raise KeyboardInterrupt
-
-            # check if the mouse pointer is on/in the window.
-            # and if so hide it.
-            focused = self.pygame.mouse.get_focused()
-            self.pygame.mouse.set_visible(not focused)
-
-    def setcaption(self, caption):
-        self.pygame.display.set_caption(caption)
-
-    def fill(self, color):
-        self.window.fill(color)
-
-    def drawblock(self, rect, color, bordercolor, borderwidth=1):
-        self.pygame.draw.rect(self.window, color, rect)
-        # draw a nice little square around so it looks more like a pixel.
-        self.pygame.draw.rect(self.window, bordercolor, rect, borderwidth)
-
-    def update(self):
-        self.pygame.display.update()
-
-    def quit(self):
-        self.pygame.quit()
+from Interfaces import OpenGlInterface
 
 
 class MatrixScreen(object):
@@ -114,7 +18,7 @@ class MatrixScreen(object):
     defined in matrix.py
     """
     def __init__(self, width, height, pixelsize, fullscreen=False,
-                 interface=PygameInterface):
+                 interface=OpenGlInterface):
         self.interface = interface(width, height, pixelsize, fullscreen)
         self.width = width
         self.height = height
