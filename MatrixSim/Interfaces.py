c3cc0ace59dc9e371a70a410dbdfdda34d15c7a8
Duality4y <201292@live.nl>
2015-03-31 17:41:30 +0200

    i have a opengl based matrix simulator working now.

--- MatrixSim/Interfaces.py
+++ MatrixSim/Interfaces.py
@@ -9,6 +9,7 @@ class Interface(object):
         # due to physical matrix layout these are switched.
         self.width = height * blocksize
         self.height = width * blocksize
+        self.blocksize = blocksize
 
     def handleinput(self):
         """
@@ -47,32 +48,63 @@ class OpenGlInterface(Interface):
     def __init__(self, width, height, pixelsize, fullscreen):
         Interface.__init__(self, width, height, pixelsize)
         glutInit()
-        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+        glutInitDisplayMode(GLUT_RGB)
         glutInitWindowSize(self.width, self.height)
         glutInitWindowPosition(self.width, self.height)
         self.window = glutCreateWindow("matrix Sim")
+        glutKeyboardFunc(self.keyboardinput)
+        if fullscreen:
+            glutFullScreen()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        glViewport(0, 0, self.width, self.height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0.0, self.width, 0.0, self.height, 0.0, 1.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
         glutMainLoopEvent()
 
-    def handleinput(self):
-        pass
+    def keyboardinput(self, *args):
+        """ special way of input handling."""
+        if(args[0] == 'q' or args[0] == '\x1b'):
+            raise SystemExit
 
     def setcaption(self, caption):
         glutSetWindowTitle(caption)
 
     def clear(self, color):
-        glViewport(0, 0, self.width, self.height)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(0.0, self.width, 0.0, self.height, 0.0, 1.0)
+        self.draw_rect((0, 0, self.width, self.height), color)
+
+    def draw_rect(self, rect, color):
+        r, g, b = color
+        r /= float(0xff)
+        b /= float(0xff)
+        g /= float(0xff)
+        x, y, width, height = rect
+        # the y direction is flipped but this fixes it :)
+        y = self.height - y - height
+
+        glColor3f(r, g, b)
+        glBegin(GL_QUADS)
+        glVertex2f(x, y)
+        glVertex2f(x + width, y)
+        glVertex2f(x + width, y + height)
+        glVertex2f(x, y + height)
+        glEnd()
 
     def drawblock(self, rect, color, bordercolor, borderwidth=1):
-        pass
+        self.draw_rect(rect, bordercolor)
+        x, y, width, height = rect
+        rect = (x + borderwidth, y + borderwidth,
+                width - borderwidth, height - borderwidth)
+        self.draw_rect(rect, color)
 
     def update(self):
-        pass
-
-    def quit(self):
-        raise SystemExit
+        glutSwapBuffers()
+        glutMainLoopEvent()
 
 
 class PygameInterface(Interface):
