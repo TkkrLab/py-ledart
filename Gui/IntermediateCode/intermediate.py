0d5815009c576fbe38058ee35b5329d95199d155
Duality4y <201292@live.nl>
2015-04-07 01:09:20 +0200

    and again i got sucked into playing with the editor, been working on the number displaying function :)

--- Gui/IntermediateCode/intermediate.py
+++ Gui/IntermediateCode/intermediate.py
@@ -84,21 +84,34 @@ numbers = [
             [0, 1, 1, 0]],
           ]
 
+class SegmentDisplay(object):
+    def __init__(self, graphics, color = (0, 0, 0xff), digits=1):
+        self.graphics = graphics
+        self.value = 0
+        self.number = []
+        self.pos = (0, 0)
+
+        self.letter_width = 4
+        self.letter_height = 7
+    def drawnumber(self, x, y, value, offset=0):
+        if value > 9:
+            return
+        for x, row in enumerate(numbers[value]):
+            for y, pixel in enumerate(row):
+                color = (0*pixel, 0*pixel, 0xff*pixel)
+                self.graphics.drawPixel(6-x, y+offset, color)
+        
 class Pattern(object):
     def __init__(self):
         self.graphics = Graphics(matrix_width, matrix_height)
-        self.letter_width = 4
-        self.letter_height = 7
         self.timer = Timer(1/2.)
         self.number = 0
+        self.segmentdisp = SegmentDisplay(self.graphics)
     def generate(self):
         self.graphics.fill(BLACK)
         if self.timer.valid():
             self.number += 1
             if self.number > 9:
                 self.number = 0
-        for x, row in enumerate(numbers[self.number]):
-            for y, pixel in enumerate(row):
-                color = (0, 0, 0xff*pixel)
-                self.graphics.drawPixel(6-x, y, color)
+        self.segmentdisp.drawnumber(0, 0, self.number)
         return self.graphics.getSurface()
