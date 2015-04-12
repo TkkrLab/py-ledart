005a014a3d09217e39e7aaa3508f2b42767618d3
Duality4y <201292@live.nl>
2015-04-12 21:28:28 +0200

    moved controller error exception class to pygamecontroller.py since it's only used there.

--- Controllers/PygameController.py
+++ Controllers/PygameController.py
@@ -1,5 +1,12 @@
 import sys
-from Controllers import ControllerError
+
+
+class ControllerError(BaseException):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
 class PygameDummyController(object):
