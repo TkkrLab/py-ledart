e2b3162828dd38bb0afe57159560f23732304044
Duality4y <201292@live.nl>
2015-04-18 20:20:00 +0200

    made a visualizer for vertical

--- Gui/written-code/vumeter_one.py
+++ Gui/written-code/vumeter_one.py
@@ -12,7 +12,7 @@ import struct
 
 audio_params = (pyaudio.paInt16, 1, 44100, True, False, 1024)
 
-select = "VisualizerTwo"
+select = "VisualizerVerti"
 
 
 def rms(buff):
@@ -312,6 +312,96 @@ class Visualizer(object):
             level = level**self.exponent
             level = int(level*0xff)
             self.graphics.drawLine(0, i, level, i, self.color)
+        return self.graphics.getSurface()
+
+    def __del__(self):
+        print("Closing Stream")
+        self.stream.close()
+        self.p.terminate()
+
+
+class VisualizerVerti(object):
+    """
+    testing with this is cool:
+    https://www.youtube.com/watch?v=82Q6DRqf9H4
+    https://youtu.be/XzjmPo6qr_0?list=RD05IZxpCWSao
+    https://youtu.be/UUIQox072QA?list=RD05IZxpCWSao
+    https://youtu.be/JTNXgzSpiTU?list=RD05IZxpCWSao
+    https://youtu.be/S5xOj3JGU0c?list=RD05IZxpCWSao
+    https://youtu.be/7Ul7uBoewdM?list=RD05IZxpCWSao
+    https://youtu.be/B1DDpyt8qyg?list=RD05IZxpCWSao
+    https://youtu.be/VwME67reIYk?list=RD05IZxpCWSao
+    https://youtu.be/hn_T2rMPL4c?list=RD05IZxpCWSao
+    https://youtu.be/U20HZoCnRGA
+    https://youtu.be/eRE0dfOfVYE
+    """
+    def __init__(self):
+        self.graphics = Graphics(matrix_width, matrix_height)
+        self.graphics.fill(BLUE)
+        self.color = BLUE
+        
+        self.timer = Timer(0)
+        
+        self.chunk = 1024
+        self.scale = 55
+        self.exponent = 2
+        self.samplerate = 44100	
+        self.audio_params = (pyaudio.paInt16, 1, self.samplerate, True, self.chunk)
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=self.audio_params[0],
+                                  channels=self.audio_params[1],
+                                  rate=self.audio_params[2],
+                                  input=self.audio_params[3],
+                                  frames_per_buffer=self.audio_params[4])
+        self.data = self.stream.read(self.chunk)
+
+    def calculate_levels(self, data, chunk, samplerate, points=6, maxi=0):
+        # Use FFT to calculate volume for each frequency
+        MAX=maxi
+         
+        # Convert raw sound data to Numpy array
+        fmt = "%dH"%(len(data)/2)
+        data2 = struct.unpack(fmt, data)
+        data2 = numpy.array(data2, dtype='h')
+         
+        # Apply FFT
+        fourier = numpy.fft.fft(data2)
+        ffty = numpy.abs(fourier[0:len(fourier)/2])/1000
+        ffty1=ffty[:len(ffty)/2]
+        ffty2=ffty[len(ffty)/2::]+2
+        ffty2=ffty2[::-1]
+        ffty=ffty1+ffty2
+        ffty=numpy.log(ffty)-2
+        fourier = list(ffty)[4:-4]
+        fourier = fourier[:len(fourier)/2]
+        size = len(fourier)
+         
+        # Add up for 6 lights
+        levels = [sum(fourier[i:(i+size/points)]) for i in xrange(0, size, size/points)][:points]
+        return levels 
+
+    def getaudio(self):
+        try:
+            raw = self.stream.read(self.audio_params[5])
+        except IOError as e:
+            if e[1] != pyaudio.paInputOverflowed:
+                raise
+            else:
+                print("Warning: audio input buffer overflow")
+            raw = '\x00' * self.audio_params[5]
+        return np.array(np.frombuffer(raw, np.int16), dtype=np.float64)
+
+    def generate(self):
+        self.graphics.fill(BLACK)
+        if self.stream.get_read_available():
+            self.data = self.stream.read(self.chunk)
+        levels = self.calculate_levels(self.data, self.chunk, self.samplerate, matrix_width)
+        for i, level in enumerate(levels):
+            self.color = color_convert(interp_color(level/max(levels)))
+            level = max(min(level/self.scale, 1.0), 0.0)
+            level = level**self.exponent
+            level = int(level*0xff)
+            self.graphics.drawLine(matrix_width-i-1, 0, matrix_width-i-1, level, self.color)
         return self.graphics.getSurface()
 
     def __del__(self):
