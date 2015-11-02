72b6cb2119af19c6d38824fda129a2efb497616e
Duality4y <201292@live.nl>
2015-11-02 01:42:28 +0100

    making great progress on costum turn know widget

--- tests/widgetTest.py
+++ tests/widgetTest.py
@@ -1,44 +1,151 @@
 import pygtk
 pygtk.require('2.0')
-import gtk
-from gtk import gdk
+import gtk, gobject
+import math
 
 
-class TurnSlider(gtk.Button):
+class GtkGraphics(object):
+    def __init__(self, cr=None):
+        self.cr = cr
+        self.fillc = (0xff, 0xff, 0xff)
+
+    def set_context(self, cr):
+        self.cr = cr
+
+    def fill(self, event, color=(0, 0, 0)):
+        if not self.cr:
+            return
+        self.cr.rectangle(event.area.x, event.area.y,
+                          event.area.width, event.area.height)
+        r, g, b = color
+        self.cr.set_source_rgb(r / 0xff, g / 0xff, b / 0xff)
+        self.cr.fill()
+
+    # function for drawing a single pixel!
+    def _draw_line(self, x1, y1, x2, y2, color=(0, 0, 0), width=1):
+        if not self.cr:
+            return
+        self.cr.set_line_width(width)
+        self.cr.move_to(x1, y1)
+        self.cr.rel_line_to(x2, y2)
+        r, g, b = color
+        self.cr.set_source_rgb(r / 0xff, g / 0xff, b / 0xff)
+        self.cr.stroke()
+        self.cr.fill()
+
+    def draw_pixel(self, x, y, color=(0, 0, 0)):
+        self._draw_line(x, y, 1, 1, color)
+
+    def draw_fill_circle(self, x, y, radius, color=(0, 0, 0), width=1):
+        if not self.cr:
+            return
+        self.cr.set_line_width(width)
+        self.cr.arc(x + radius, y + radius, radius, 0, 2 * math.pi)
+        r, g, b = color
+        self.cr.set_source_rgb(r / 0xff, g / 0xff, b / 0xff)
+        self.cr.fill()
+
+    def draw_circle(self, x, y, radius, color=(0, 0, 0), width=1):
+        if not self.cr:
+            return
+        self.draw_fill_circle(x, y, radius, color=(0, 0, 0))
+        self.draw_fill_circle(x + width, y + width,
+                              radius - width, color=(0xff, 0xff, 0xff))
+        self.cr.fill()
+
+    def draw_line(self, x1, y1, x2, y2, color=(0, 0, 0), width=1):
+        if not self.cr:
+            return
+        self.cr.set_line_width(width)
+        self.cr.move_to(x1, y1)
+        self.cr.rel_line_to(x2, y2)
+        r, g, b = color
+        self.cr.set_source_rgb(r / 0xff, g / 0xff, b / 0xff)
+        self.cr.stroke()
+        self.cr.fill()
+
+    def draw_rect(self, x, y, width, height, color=(0, 0, 0), lwidth=1):
+        # for w in range(0, lwidth):
+        for xr in range(x, x + width):
+            self.draw_pixel(xr, y)
+            self.draw_pixel(xr, y + height - 1)
+        for yr in range(y, y + height):
+            self.draw_pixel(x, yr)
+            self.draw_pixel(x + width - 1, yr)
+
+
+class TurnSlider(gtk.DrawingArea):
     def __init__(self, *args, **kwds):
-        gtk.Button.__init__(self)
-        gtk.Button.connect(self, "pressed", self.pressed)
-        gtk.Button.connect(self, "released", self.released)
-        gtk.Button.connect(self, "motion-notify-event", self.evented)
-        gtk.Button.set_events(self, gdk.POINTER_MOTION_MASK)
-        self.props.relief = gtk.RELIEF_NONE
+        gtk.DrawingArea.__init__(self)
+        gtk.DrawingArea.set_size_request(self, 100, 100)
+        flags = (gtk.gdk.BUTTON_RELEASE_MASK |
+                 gtk.gdk.BUTTON_PRESS_MASK |
+                 gtk.gdk.POINTER_MOTION_MASK)
+        gtk.DrawingArea.set_events(self, flags)
+        gtk.DrawingArea.connect(self, 'expose-event',
+                                self.on_draw_area_expose)
+        gtk.DrawingArea.connect(self, 'motion-notify-event',
+                                self.motion)
+        gtk.DrawingArea.connect(self, 'button-press-event',
+                                self.button_handle)
+        gtk.DrawingArea.connect(self, 'button-release-event',
+                                self.button_handle)
+        self.graphics = GtkGraphics()
 
-        self.image = gtk.Image()
-        self.image.set_from_file('/home/robert/py-artnet/hacked.png')
-        self.image.show()
-        self.add(self.image)
-
-        self.lmousedown = False
+        self.knobradius = 15
+        self.lmpressed = False
         self.mpos = (0, 0)
+        self.press_y = 0
+        self.release_y = 0
 
-    def evented(self, widget, event):
+    def redraw(self):
+        self.queue_draw()
+
+    def on_draw_area_expose(self, widget, event, data=None):
+        self.cr = widget.window.cairo_create()
+        self.graphics.set_context(self.cr)
+        self.graphics.fill(event, (0xff, 0xff, 0xff))
+        x, y = (event.area.width / 2 - self.knobradius,
+                event.area.height / 2 - self.knobradius)
+        self.graphics.draw_circle(x, y, self.knobradius)
+
+    # use motion of mouse when pressed to update knob position
+    def motion(self, widget, event):
         self.mpos = (event.x, event.y)
+        self.redraw()
         return True
 
-    def pressed(self, widget, data=None):
+    def knob_drag(self, widget, event, data=None):
         print("pressed!", self.mpos)
-        self.lmousedown = True
+        print(event)
+        y = event.y
+        type = event.type
+        button = event.button
+        if button == 1:
+            if type == gtk.gdk.BUTTON_PRESS:
+                self.press_y = y
+            elif type == gtk.gdk.BUTTON_RELEASE:
+                self.release_y = y
+                print("dragged: %d" % (self.press_y - self.release_y))
 
-    def released(self, widget, data=None):
-        self.lmousedown = False
+    # keep track of which button is pressed
+    # what each button does
+    def button_handle(self, widget, event, data=None):
+        # handle knop turning if any.
+        self.knob_drag(widget, event, data)
 
 
 class Gui(object):
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.connect("destroy", self.destroy)
+
+        self.windowbox = gtk.HBox()
+
         self.turnslider = TurnSlider()
-        self.window.add(self.turnslider)
+        self.windowbox.pack_start(self.turnslider, False, False, 0)
+        self.windowbox.show()
+        self.window.add(self.windowbox)
         self.turnslider.show()
         self.window.show()
 
