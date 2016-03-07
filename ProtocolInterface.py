82ccb7ac2bf3d84d786b1760d6787749ba129bda
Duality4y <201292@live.nl>
2016-03-08 00:24:57 +0100

    minor commit

--- ProtocolInterface.py
+++ ProtocolInterface.py
@@ -28,9 +28,9 @@ class Interface(object):
         readable, writable, exceptional = select.select([self.sock],
                                                         [self.sock],
                                                         [self.sock])
-        for w in writable:
-            w.sendto(data, target)
-        # self.sock.sendto(data, target)
+        # for w in writable:
+            # w.sendto(data, target)
+        self.sock.sendto(data, target)
 
     def send(self, data, ip):
         """
