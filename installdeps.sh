77c71c5274aed576ef4cac4c0b47138b6be4e8b9
Duality4y <201292@live.nl>
2016-08-01 18:06:50 +0200

    added installdeps.sh this install through pip dependencies for py-ledart

--- installdeps.sh
+++ installdeps.sh
@@ -3,9 +3,6 @@ pip install svn+https://svn.code.sf.net/p/python-xlib/code/trunk/
 pip install git+https://github.com/SavinaRoja/PyUserInput
 pip install git+https://github.com/CTPUG/pygame_cffi
 pip install av
-pip install cffi
-pip install greenlet
 pip install pillow
 pip install pyopengl
 pip install pyserial
-pip install readline
