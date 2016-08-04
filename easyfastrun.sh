dc9b1502f12aea6114aaf94dc99f2a7b78b0e4a4
Duality4y <201292@live.nl>
2016-08-04 20:42:26 +0200

    working on making it a module

--- easyfastrun.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-pypy runPatternJob.py --conf=pattern_conf.py --fps=0 --netProtocol=lmcp --showFps=enabled --color=enabled
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ledart',
+      version='0.2',
+      description='led controle software.',
+      url='https://github.com/tkkrlab/py-ledart',
+      author='Duality (aka Robert van der Tuuk',
+      author_email='201292@live.nl',
+      license='GPL3',
+      packages=['ledart'],
+      zip_safe=False)
