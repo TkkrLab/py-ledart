da1289e5da9f3e45714c38101f16e9c377a2428e
Duality4y <201292@live.nl>
2016-08-06 18:57:25 +0200

    been editing setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='ledart',
       author='Duality (aka Robert van der Tuuk',
       author_email='201292@live.nl',
       license='GPL3',
-      packages=['ledart'],
+      packages=['Ledart'],
       zip_safe=False)
