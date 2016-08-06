1b1ec7d6549edcc18713c585bb56a256674af0b0
Duality4y <201292@live.nl>
2016-08-06 20:04:40 +0200

    seeing if i can make setup use requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(requirements):
+    with open(requirements) as f:
+        return [l.strip('\n') for l in f if l.strip('\n') and not l.startswith('#')]
+
+reqs = parse_requirements("Ledart/requirements.txt")
+
 setup(name='ledart',
       version='0.2',
       description='led controle software.',
@@ -8,4 +14,5 @@ setup(name='ledart',
       author_email='201292@live.nl',
       license='GPL3',
       packages=find_packages(),
+      install_requires=reqs,
       zip_safe=False)
