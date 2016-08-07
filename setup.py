6807dcf41e97a754264d05c3999081e277e5641f
Duality <201292@live.nl>
2016-08-07 03:19:44 +0200

    a branch save

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 from distutils.core import setup
 from distutils.command.install_data import install_data
 from setuptools import find_packages
-from post_setup import main as post_install
-
-
-class my_install(install_data):
-    def run(self):
-        install_data.run(self)
-        # post_install()
 
 setup(name='ledart',
       version='0.2',
@@ -18,5 +11,4 @@ setup(name='ledart',
       license='GPL3',
       packages=find_packages(),
       install_requires=['pip'],
-      cmdclass={'install': my_install},
       zip_safe=False)
