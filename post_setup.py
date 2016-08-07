606c59b3b8ce20f7514d8771e199930c6126bf2d
Duality <201292@live.nl>
2016-08-07 03:57:37 +0200

    setup reads requiremenst file and see if it knows links

--- post_setup.py
+++
@@ -1,8 +0,0 @@
-import subprocess as sp
-
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
--- setup.py
+++ setup.py
@@ -2,7 +2,21 @@ from distutils.core import setup
 from distutils.command.install_data import install_data
 from setuptools import find_packages
 
-setup(name='ledart',
+reqs = []
+links = []
+
+with open("Ledart/requirements") as f:
+    for line in f:
+        line = line.strip()
+        if line.startswith("#"):
+            continue
+        elif "+" in line:
+            line = line[line.index("+") + 1:]
+            links.append(line)
+        else:
+            reqs.append(line)
+
+setup(name='Leddart',
       version='0.2',
       description='led controle software.',
       url='https://github.com/tkkrlab/py-ledart',
@@ -10,5 +24,6 @@ setup(name='ledart',
       author_email='201292@live.nl',
       license='GPL3',
       packages=find_packages(),
-      install_requires=['pip'],
+      install_requires=reqs,
+      dependency_links=links,
       zip_safe=False)
