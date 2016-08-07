f11a552ac288500d50fb60d7e7df02bb0e5b825c
Duality4y <201292@live.nl>
2016-08-08 00:38:26 +0200

    apperently requirements.txt doesn't get delivered

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@ from distutils.core import setup
 from distutils.command.install_data import install_data
 from setuptools import find_packages
 
-reqs = []
-links = []
-
-with open("Ledart/requirements.txt") as f:
-    for line in f:
-        line = line.strip()
-        if line.startswith("#"):
-            continue
-        elif "+" in line:
-            line = line[line.index("+") + 1:]
-            links.append(line)
-        else:
-            reqs.append(line)
+links = ['Pillow==3.3.0',
+         'PyOpenGL==3.1.0',
+         'PyUserInput==0.1.10',
+         'cffi==1.7.0',
+         'greenlet==0.4.9',
+         'pygame-cffi==0.1.1',
+         'pyserial==3.1.1',
+         'python-xlib==0.16',
+         'readline==6.2.4.1',
+         'six==1.10.0',
+         'wsgiref==0.1.2']
 
 setup(name='Ledart',
       version='0.2',
