dc750284f4efed406aead53754c834b4e2ff456d
Duality <201292@live.nl>
2016-09-19 21:03:20 +0200

    forgot comma seperators in a list

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ import os
 with os.popen("git describe --abbrev=0") as fd:
     version = fd.read().strip()
 
-reqs = ['Pillow >= 3.3.0'
-        'PyOpenGL >= 3.1.0'
-        'PyUserInput >= 0.1.10'
-        'cffi >= 1.7.0'
-        'pygame-cffi >= 0.1.1'
-        'pyserial >= 3.1.1'
-        'python-xlib >= 0.16'
-        'pyalsaaudio >= 0.8.2']
+reqs = ['Pillow >= 3.3.0',
+        'PyOpenGL >= 3.1.0',
+        'PyUserInput >= 0.1.10',
+        'cffi >= 1.7.0',
+        'pygame-cffi >= 0.1.1',
+        'pyserial >= 3.1.1',
+        'python-xlib >= 0.16',
+        'pyalsaaudio >= 0.8.2',]
 
 setup(name='Ledart',
       version=version,
