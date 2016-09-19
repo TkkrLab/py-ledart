aefebb5259ec10795e6f51178a4a0f846b7b9c1c
Duality <201292@live.nl>
2016-09-19 22:06:44 +0200

    been busy reorganizing code and moving it around a bit.

--- Ledart/runPatternJob.py
+++ Ledart/runPatternJob.py
@@ -2,7 +2,6 @@
 
 # import some generaly used libraries
 import os
-import gc
 import sys
 import imp
 import time
@@ -13,74 +12,14 @@ from socket import gethostbyname
 
 import cProfile
 
-# import matrix simulator and matrix specifics
 from Tools.Graphics import Surface
-
+from utils import load_targets
+from utils import find_patterns_in_dir
 from ArgumentParser import get_args
 # get command line arguments
 args = get_args()
-protocol = None
-matrixsim = None
-targets = None
-
-def get_trace():
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    fmt = (exc_type, fname, exc_tb.tb_lineno)
-    fmtstr = "%s:%s:%s" % fmt
-    return fmtstr
-
-
-def find_patterns_in_dir(dir):
-    patterns = []
-    # get the current working directory so we.
-    # can join and find it.
-    dir = os.path.join(os.getcwd(), dir)
-    # see if dir is already in path. else add it.
-    if dir not in sys.path:
-        sys.path.append(dir)
-    else:
-        print("directory in path.")
-    # for everything in a directory.
-    for item in os.listdir(dir):
-        # if it is a source file.
-        if item.endswith(".py") and not item.startswith("__"):
-            # extract the file name and import it.
-            sfile = item.split('.')[0]
-            try:
-                mod = __import__(sfile)
-            except Exception as e:
-                print("%s:Couldn't import module cause: %s" % (sfile, e))
-                traceback.print_exc()
-                continue
-            # extract classes
-            classes = get_pattern_classes(mod)
-            # if any found:
-            if classes:
-                # append the object to patterns
-                patterns += classes
-    # return the patterns
-    return list(set(patterns))
-
-
-def get_pattern_classes(module):
-    # holds the patterns that are found
-    patterns = []
-    # look in the module dictionary for objects with generate function
-    for obj in module.__dict__:
-        # if we find objects
-        if isinstance(obj, object):
-            try:
-                # we try and get that objects dictionary.
-                # if it's a class it will contain methods and more.
-                obj_dict = module.__dict__[obj].__dict__
-                if(obj_dict['generate']):
-                    patterns.append(module.__dict__[obj])
-            except:
-                # continue if we try and read something we can't.
-                continue
-    # return a list of classes that have a generate function in them
-    return patterns
+# load config
+targets, protocol, matrixscreen = load_targets(args.config)
 
 
 def tst_patterns(directory, showpass=True):
@@ -100,38 +39,15 @@ def tst_patterns(directory, showpass=True):
             print("---------------------")
             continue
 
-""" TODO: use actuall config file and not python source files. """
-def load_targets(configfile):
-    # this function allows loading of the config files specified by
-    # --config=configfile and load patterns defined in there.
-
-    # test if the config file exists, if not it's maybe a local file
-    # and else it's probably a path description + file.
+def listpatterns():
     currentdir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    variables = {}
-    variables['basedir'] = currentdir
-    
-    if not os.path.exists(configfile):
-        configfile = os.path.join(currentdir, "configs", configfile)
-    
-    print("loading config from: %s" % (configfile))
-    execfile(configfile, variables)
-
-    targets = variables.get('targets', None)
-    protocol = variables.get('protocol', None)
-    matrix_sim = variables.get('matrixsim', None)
-
-    print("protocol: %s" % protocol)
-    print("matrixsim: %s" % matrix_sim)
-
-    return (targets, protocol, matrix_sim)
-
-
-def checkList(first, second):
-    for item1, item2 in zip(first, second):
-        if item1 != item2:
-            return False
-    return True
+    pattern_objects = find_patterns_in_dir(os.path.join(currentdir, 'Patterns'))
+    patterns = []
+    for pattern in pattern_objects:
+        patterns.append(pattern.__name__)
+    # print a sorted list of patterns.
+    for pattern in sorted(patterns):
+        print(pattern)
 
 
 def sendout(args, targets, protocol):
@@ -164,17 +80,6 @@ def sendout(args, targets, protocol):
         cleanup(8)
 sendout.previous = Surface(width=10, height=10)
 
-
-def listpatterns():
-    currentdir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    pattern_objects = find_patterns_in_dir(os.path.join(currentdir, 'Patterns'))
-    patterns = []
-    for pattern in pattern_objects:
-        patterns.append(pattern.__name__)
-    # print a sorted list of patterns.
-    for pattern in sorted(patterns):
-        print(pattern)
-
 # cleanup exits calls close and leaves tty in sane state.
 def cleanup(d):
     print("\nExiting(%d) closing connections." % d)
@@ -195,8 +100,6 @@ def main():
     # make tty be sane so that if the tty/terminal screws up.
     # this will make it workable again.
     signal.signal(signal.SIGINT, sigint_handler)
-    # load config
-    targets, protocol, matrixscreen = load_targets(args.config)
 
     # command parsing
     if args.list:
@@ -248,6 +151,3 @@ def main():
             else:
                 sendout(args, targets, protocol)
             previousTime = currentTime
-
-if __name__ == "__main__":
-    main()
--- Ledart/utils.py
+++ Ledart/utils.py
@@ -1,7 +1,9 @@
+import os, sys
+
+
 """
     these functions are utils, that are handy to have for various things.
 """
-
 
 def chunked(data, chunksize):
     """
@@ -25,9 +27,101 @@ def chunks(l, n):
     for i in xrange(0, len(l), n):
         yield l[i:i + n]
 
-
 def to_matrix(l, n):
     """
     turns a list l into a 2d list with inner list size of n
     """
     return [l[i:i + n] for i in range(0, len(l), n)]
+
+""" prints out a trace """
+def get_trace():
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    fmt = (exc_type, fname, exc_tb.tb_lineno)
+    fmtstr = "%s:%s:%s" % fmt
+    return fmtstr
+
+""" returns a list of objects found in a directory/module """
+def find_patterns_in_dir(dir):
+    patterns = []
+    # get the current working directory so we.
+    # can join and find it.
+    dir = os.path.join(os.getcwd(), dir)
+    # see if dir is already in path. else add it.
+    if dir not in sys.path:
+        sys.path.append(dir)
+    else:
+        print("directory in path.")
+    # for everything in a directory.
+    for item in os.listdir(dir):
+        # if it is a source file.
+        if item.endswith(".py") and not item.startswith("__"):
+            # extract the file name and import it.
+            sfile = item.split('.')[0]
+            try:
+                mod = __import__(sfile)
+            except Exception as e:
+                print("%s:Couldn't import module cause: %s" % (sfile, e))
+                traceback.print_exc()
+                continue
+            # extract classes
+            classes = get_pattern_classes(mod)
+            # if any found:
+            if classes:
+                # append the object to patterns
+                patterns += classes
+    # return the patterns
+    return list(set(patterns))
+
+""" returns a list of objects that contain a generate function. """
+def get_pattern_classes(module):
+    # holds the patterns that are found
+    patterns = []
+    # look in the module dictionary for objects with generate function
+    for obj in module.__dict__:
+        # if we find objects
+        if isinstance(obj, object):
+            try:
+                # we try and get that objects dictionary.
+                # if it's a class it will contain methods and more.
+                obj_dict = module.__dict__[obj].__dict__
+                if(obj_dict['generate']):
+                    patterns.append(module.__dict__[obj])
+            except:
+                # continue if we try and read something we can't.
+                continue
+    # return a list of classes that have a generate function in them
+    return patterns
+
+""" loads variables from a config file, that is actually a python file."""
+def load_targets(configfile):
+    # this function allows loading of the config files specified by
+    # --config=configfile and load patterns defined in there.
+
+    # test if the config file exists, if not it's maybe a local file
+    # and else it's probably a path description + file.
+    currentdir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    variables = {}
+    variables['basedir'] = currentdir
+    
+    if not os.path.exists(configfile):
+        configfile = os.path.join(currentdir, "configs", configfile)
+    
+    print("loading config from: %s" % (configfile))
+    execfile(configfile, variables)
+
+    targets = variables.get('targets', None)
+    protocol = variables.get('protocol', None)
+    matrix_sim = variables.get('matrixsim', None)
+
+    print("protocol: %s" % protocol)
+    print("matrixsim: %s" % matrix_sim)
+
+    return (targets, protocol, matrix_sim)
+
+def checkList(first, second):
+    for item1, item2 in zip(first, second):
+        if item1 != item2:
+            return False
+    return True
+
