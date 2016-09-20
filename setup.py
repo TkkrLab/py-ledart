67398a9a1a230f2487d5369627f7be1b1acc79b1
Duality <201292@live.nl>
2016-09-21 00:57:11 +0200

    been writing tests way more to go!

--- setup.py
+++ setup.py
@@ -11,14 +11,18 @@ import os
 with os.popen("git describe --abbrev=0") as fd:
     version = fd.read().strip()
 
-reqs = ['Pillow >= 3.3.0',
-        'PyOpenGL >= 3.1.0',
-        'PyUserInput >= 0.1.10',
-        'cffi >= 1.7.0',
-        'pygame-cffi >= 0.1.1',
-        'pyserial >= 3.1.1',
-        'python-xlib >= 0.16',
-        'pyalsaaudio >= 0.8.2',]
+with open("requirements.txt") as f:
+    reqs = [line.strip() for line in f.readlines()]
+# print("reqs: ", reqs)
+
+# reqs = ['Pillow >= 3.3.0',
+#         'PyOpenGL >= 3.1.0',
+#         'PyUserInput >= 0.1.10',
+#         'cffi >= 1.7.0',
+#         'pygame-cffi >= 0.1.1',
+#         'pyserial >= 3.1.1',
+#         'python-xlib >= 0.16',
+#         'pyalsaaudio >= 0.8.2',]
 
 setup(name='Ledart',
       version=version,
