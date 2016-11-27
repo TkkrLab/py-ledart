eadf9c71942fc9cac2b6e91f230118dec27b252a
Robert van der Tuuk <201292@live.nl>
2016-11-27 22:11:58 +0100

    removed a print statement

--- Ledart/Tools/Graphics/Imaged.py
+++ Ledart/Tools/Graphics/Imaged.py
@@ -13,7 +13,6 @@ class ImageSurface(Surface):
         backup_fname = os.path.join(base_path, 'image/tkkrlab.png')
 
         fname = kwargs.get('fname', backup_fname)
-        print("fname: " + fname)
 
         image = Image.open(fname)
         self.imtype = self.determine_type(fname)
