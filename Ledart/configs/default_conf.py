48fa4e59f832a35052c9367fc63bf494a56fdc80
Robert van der Tuuk <201292@live.nl>
2016-11-27 20:23:35 +0100

    the default config works too now

--- Ledart/configs/default_conf.py
+++ Ledart/configs/default_conf.py
@@ -1,26 +1,25 @@
 # import all patterns availble for use.
 from Ledart.Lmcp import *
 from Ledart.Artnet import *
-from Ledart.stripinfo import *
+from Ledart.utils import *
 from Ledart.MatrixSim.MatrixScreen import MatrixScreen, interface_opts
 
 localhost = "localhost"
 dest = localhost
 
 width, height = 32, 32
-set_strip_dimensions(matrix(0, 0, width, height))
+dims = matrix(0, 0, width, height)
 
 # don't send out anything.
 protocol = None
 # start the simulator for a nice demo.
-matrixsim = MatrixScreen(width=width,
-                         height=height,
-                         pixelsize=10,
+matrixsim = MatrixScreen(dims=dims,
+                         pixelsize=7,
                          fullscreen=False,
                          interface=interface_opts["pygame"])
 
 from Ledart.Patterns.Patterns import *
 
 targets = {
-    dest: PixelLife(color=(0, 0, 0xff)),
+    dest: PixelLife(dims=dims, color=(0, 0, 0xff)),
 }
