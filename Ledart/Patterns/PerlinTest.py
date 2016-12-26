90db2935394c6b7c1cf4af75f6f040b002ede913
Robert van der Tuuk <201292@live.nl>
2016-12-26 22:55:23 +0100

    the perlin noise has color now.

--- Ledart/Patterns/PerlinTest.py
+++ Ledart/Patterns/PerlinTest.py
@@ -4,15 +4,21 @@ from Ledart import Vector
 
 import noise
 import random
+import colorsys
 
 class PerlinTest(Graphics):
     def __init__(self, **kwargs):
         Graphics.__init__(self, **kwargs)
         self.t = 0
+        self.y_offset = random.randint(0, self.height)
 
     def generate(self):
-        self.fill(BLACK)
-        x = noise.pnoise1(self.t)
-        x = translate(x, 0, 1., 10, self.width - 10)
-        self.draw_circle(x, self.height / 2, 10, GREEN)
-        self.t += 0.01
+        # self.fill(BLACK)
+        x = noise.pnoise1(self.t) + 1
+        y = noise.pnoise1(self.t + self.y_offset) + 1
+        x = translate(x, 0, 2., 10, self.width - 10)
+        y = translate(y, 0, 2., 10, self.height - 10)
+        # self.draw_circle(x, y, 10, GREEN)
+        color = [int(0xff * c) for c in colorsys.hsv_to_rgb(self.t, 1, 1)]
+        self.draw_pixel(x, y, color)
+        self.t += 0.001
